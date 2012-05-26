cfe06db848a5cd1a98b2872d67d157f59c3ef737
Joe Fearnley <joe.fearnley@gmail.com>
2012-05-25 22:58:55 -0400

    Added some more clips

--- js/script.js
+++ js/script.js
@@ -6,13 +6,16 @@ $(document).ready(function() {
     var element = $(this);
     var icon = element.children("i");
     icon.removeClass("icon-play").addClass("icon-pause");
+
     var player = $("#player").jPlayer({
       swfPath: "jplayer",
       supplied: "mp3",
     });
+
     player.jPlayer("setMedia", {
       mp3: "audio/" + element.attr("id") + ".mp3"
     });
+
     player.jPlayer("play");
     icon.removeClass("icon-pause").addClass("icon-play");
   });
