821c8c6286bd124061abfecf77f7b182c6171bdd
Joe Fearnley <joe.fearnley@gmail.com>
2013-01-09 22:32:27 -0500

    Reverting a bunch of files on master.

--- js/script.js
+++ js/script.js
@@ -2,44 +2,23 @@
  * Author: Joe Fearnley
  */
 $(document).ready(function() {
-
-    var player = $("#player").jPlayer({
-      preload: "auto",
-      swfPath: "js/jplayer",
-      supplied: "mp3",
-      solution:"flash,html",
-    /*
-      ready: function() {
-        icon.removeClass("icon-play").addClass("icon-pause");
-        console.log("play to pause");
-      },
-      */
-      ended: function() {
-        icon.removeClass("icon-pause").addClass("icon-play");
-        console.log("pause to play");
-        $(".clip").children("i").removeClass("icon-pause").addClass("icon-play");
-      }
-    });
-
   $(".clip").click(function() {
     var element = $(this);
     var icon = element.children("i");
-    console.log("adding pause");
     icon.removeClass("icon-play").addClass("icon-pause");
-    //icon.addClass("icon-pause");
+
+    var player = $("#player").jPlayer({
+      swfPath: "js/jplayer",
+      supplied: "mp3",
+    });
 
     player.jPlayer("setMedia", {
       mp3: "audio/" + element.attr("id") + ".mp3"
     });
 
-    player.jPlayer("play", function() {
-      console.log("player done asdfasdf");
-      icon.removeClass("icon-pause").addClass("icon-play");
-    });
+    player.jPlayer("play");
+    icon.removeClass("icon-pause").addClass("icon-play");
 
-    //console.log("adding play");
-    //icon.removeClass("icon-pause").addClass("icon-play");
-    //
-    //return false;
+    return false;
   });
 });
