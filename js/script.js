43753cefefc9b382e3806058aecd766b25e69058
Joe Fearnley <joe.fearnley@gmail.com>
2013-01-09 23:01:51 -0500

    Adding back in modernizr files.

--- js/script.js
+++ js/script.js
@@ -1,7 +1,24 @@
-/*
- * Author: Joe Fearnley
- */
-$(document).ready(function() {
+(function($){
+ 
+  var clips = [
+    { filname: "wheres-the-buffet", title: "Where's the Buffet?" },
+    { filname: "hot-damn", title: "Hot Damn" },
+    { filname: "pansy-immune-system", title: "Pansy Immune System" },
+    { filname: "upper-football", title: "Upper Football" },
+    { filname: "william-butterfield", title: "William Butterfield" },
+    { filname: "smooth-as-silk", title: "Smooth as Silk" },
+    { filname: "black-metal", title: "Black Metal" },
+    { filname: "large-can-of-beer", title: "Large Can of Beer" },
+    { filname: "bought-a-new-part", title: "Bought a New Part" },
+    { filname: "who-is-this", title: "Who is this?" },
+    { filname: "futons-instead-of-pews", title: "Futons Instead of Pews" },
+    { filname: "challenge-to-a-duel", title: "Challenge to a Dual" },
+    { filname: "hahaha", title: "Hahahaha" },
+    { filname: "where-you-going", title: "Where You Going?" },
+    { filname: "pessimistic", title: "Pessimistic" },
+    { filname: "clamy-feeling", title: "Clamy Feeling" }
+  ];
+ 
   $(".clip").click(function() {
     var element = $(this);
     var icon = element.children("i");
@@ -21,4 +38,5 @@ $(document).ready(function() {
 
     return false;
   });
-});
+
+})(jQuery);
