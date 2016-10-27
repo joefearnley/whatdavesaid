9bbfc017ad838cde58a61e529c14dc0c79831c51
Joe Fearnley <joe.fearnley@gmail.com>
2016-10-26 22:04:27 -0400

    Removing more of the uneeded and unused

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-document.querySelectorAll('button.clip')
-    .forEach(function(el) {
-        el.addEventListener('click', function() {
-            var fileName = '/audio/' + this.getAttribute('data-file') + '.mp3';
-            var clip = new Audio(fileName);
-            clip.play();
-        });
-    });
